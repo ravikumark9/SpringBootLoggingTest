9bde9b4f736c33f7b23cde2bb9a52bd1ca327876
vellianm <vellianm@PC0QRLDA.one.ads.bms.com>
2018-04-10 18:00:02 +0530

    Init commit

---
+++ src/main/java/com/mohan/app/SpringBootLoggingTestApplication.java
@@ -0,0 +1,29 @@
+package com.mohan.app;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class SpringBootLoggingTestApplication {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(SpringBootLoggingTestApplication.class);
+	
+    public static void main(String[] args) {
+		SpringApplication.run(SpringBootLoggingTestApplication.class, args);
+	}
+    
+    @RequestMapping("/")
+    public String getString() {
+      for(int i = 0; i<10000; i++) {
+        LOGGER.info("Test"); 
+      }
+      return "Hello!!";
+    }
+	
+	
+}
