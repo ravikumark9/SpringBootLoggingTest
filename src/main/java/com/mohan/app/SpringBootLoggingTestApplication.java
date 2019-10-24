e09b512d71058d622d0700c7cd12c690a2365233
Velliangiri <mohanprabu.velliangiri@bms.com>
2019-10-24 16:12:34 +0530

    Fixing deployment issue

--- src/main/java/com/mohan/app/SpringBootLoggingTestApplication.java
+++ src/main/java/com/mohan/app/SpringBootLoggingTestApplication.java
@@ -4,18 +4,25 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @SpringBootApplication
-public class SpringBootLoggingTestApplication {
+public class SpringBootLoggingTestApplication extends SpringBootServletInitializer {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(SpringBootLoggingTestApplication.class);
 	
     public static void main(String[] args) {
 		SpringApplication.run(SpringBootLoggingTestApplication.class, args);
 	}
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(SpringBootLoggingTestApplication.class);
+    }
     
     @RequestMapping("/")
     public String getString() {
